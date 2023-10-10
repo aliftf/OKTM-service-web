b6fae8790ea1436d48fcf59d4bd581eb8aec64e6
Alif T <alift929@gmail.com>
2023-10-10 16:47:20 +0700

    initiate bootstrap, main layout, and navbar

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>OKTM Service</title>
+</head>
+<body>
+    
+    @include('partials.navbar')
+
+    <h1>HELLO</h1>
+    <div class="container">
+        @yield('container')
+    </div>
+
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
