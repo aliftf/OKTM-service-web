b3fb7474a5f4609a0ae776c126572ede40d99783
Regyyyy <regyrahman@gmail.com>
2023-10-25 08:10:26 +0700

    Container form
    Desain container form

--- resources/views/verifikasiPengajuanKtm.blade.php
+++ resources/views/verifikasiPengajuanKtm.blade.php
@@ -2,8 +2,16 @@
 
 @section('container')
 
-<div class="container-fluid">
-    
+<div class="container-fluid" 
+style="margin-top:300px; padding-left:250px; padding-right:250px">
+    <div class="container-fluid border rounded-4 bg-light">
+        <h1>INI TEST</h1>
+        <h2>Ini juga test</h2>
+        <h2>Ini juga test</h2>
+        <h2>Ini juga test</h2>
+        <h2>Ini juga test</h2>
+        <h2>Ini juga test</h2>
+    </div>
 </div>
 
 @endsection
