1a2268ef7289002ee5a669067d45717416ca7c51
raimess11 <rahma.sakti24@gmail.com>
2023-10-28 00:27:26 +0700

    melengkapi form

--- resources/views/pengajuanBermasalah.blade.php
+++ resources/views/pengajuanBermasalah.blade.php
@@ -92,56 +92,51 @@
                 <div>
                 <form>
                     <div class="form-group row ">
-                        <label for="inputEmail3" class="col-lg-2 col-form-label">Email</label>
-                        <div class="col-lg-10">
-                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                        <label for="inputNama" class="col-lg-2 col-form-label">Nama</label>
+                        <div class="col-lg-5">
+                        <input type="Text" class="form-control" id="inputNama" placeholder="Nama">
                         </div>
                     </div>
                     <div class="form-group row pt-2">
-                        <label for="inputPassword3" class="col-lg-2 col-form-label">Password</label>
-                        <div class="col-lg-10">
-                        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                        <label for="inputTahun" class="col-lg-2 col-form-label">Tahun Ajaran</label>
+                        <div class="col-lg-5">
+                        <input type="Text" class="form-control" id="inputTahun" placeholder="Tahun Ajaran">
                         </div>
                     </div>
-                    <fieldset class="form-group pt-2">
-                        <div class="row">
-                        <legend class="col-form-label col-lg-2 pt-0">Radios</legend>
-                        <div class="col-lg-10 pt-2 pt-2">
-                            <div class="form-check">
-                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
-                            <label class="form-check-label" for="gridRadios1">
-                                First radio
-                            </label>
-                            </div>
-                            <div class="form-check">
-                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
-                            <label class="form-check-label" for="gridRadios2">
-                                Second radio
-                            </label>
-                            </div>
-                            <div class="form-check disabled">
-                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
-                            <label class="form-check-label" for="gridRadios3">
-                                Third disabled radio
-                            </label>
-                            </div>
-                        </div>
-                        </div>
-                    </fieldset>
-                    <div class="form-group row">
-                        <div class="col-sm-2">Checkbox</div>
-                        <div class="col-sm-10">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="gridCheck1">
-                            <label class="form-check-label" for="gridCheck1">
-                            Example checkbox
-                            </label>
-                        </div>
+                    <div class="form-group row pt-2">
+                        <label for="inputProgram" class="col-lg-2 col-form-label">Program Study</label>
+                        <div class="col-lg-5">
+                        <input type="Text" class="form-control" id="inputProgram" placeholder="Program Study">
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <div class="col-sm-10">
-                        <button type="submit" class="btn btn-primary">Sign in</button>
+                    <div class="form-group row pt-2">
+                        <label for="inputKSM" class="col-lg-2 col-form-label">Upload KSM</label>
+                        <div class="col-lg-5">
+                        <input type="file" class="form-control" id="inputKSM" placeholder="KSM">
+                        </div>
+                    </div>
+                    <div class="form-group row pt-2">
+                        <label for="inputKSM" class="col-lg-2 col-form-label">Upload KTM</label>
+                        <div class="col-lg-5">
+                        <input type="file" class="form-control" id="inputKTM" placeholder="KTM">
+                        </div>
+                    </div>
+                    <div class="form-group row pt-2 ">
+                        <label for="inputBukti" class="col-lg-2 col-form-label">Upload Bukti Pembayaran</label>
+                        <div class="col-lg-5">
+                            <div class="d-flex justify-content-center">
+                            <input type="file" class="form-control" id="inputBukti" placeholder="Bukti">
+                            </div>
+
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row pt-5">
+                        <div class="col-sm-12 col-lg-8">
+                            <div class="d-flex justify-content-end">
+                                <button type="submit" class="btn btn-danger" style="width:200px">Submit</button>
+
+                            </div>
                         </div>
                     </div>
                     </form>
