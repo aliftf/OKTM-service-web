72b92044b66aab0bea6f2c0358481fa1a66e9d6b
Regyyyy <regyrahman@gmail.com>
2024-01-03 04:11:07 +0700

    Penambahan sorting pada header tabel finalisasi dan verifikasi

--- resources/views/finalisasiPengajuan.blade.php
+++ resources/views/finalisasiPengajuan.blade.php
@@ -7,7 +7,7 @@
     <h2 class="pt-5 fw-bold">Finalisasi</h2>
     <!-- Table -->
     <div class="py-5">
-        <table class="table-list table-responsive table table-header text-center table-borderless hasil-table shadow">
+        <table id="dataTable" class="table-list table-sortable table-responsive table table-header text-center table-borderless hasil-table shadow">
             <thead class="h5 fw-bold align-middle m">
                 <tr>
                     <th class="w-25">Nama Mahasiswa</th>
@@ -22,7 +22,7 @@
                         <td><div class="py-3 px-2 border-end border-3">{{$data->mahasiswa->nama}}</div></td>
                         <td><div class="py-3 px-2 border-end border-3">{{$data->tanggal}}</div></td>
                         <td><div class="py-3 px-2 border-end border-3">{{$data->tipe}}</div></td>
-                        <td><div class="py-3 px-2"><button type="button" class="shadow-sm btn btn-danger btn-lg fw-bold border rounded-lg" id="modalbutton">Done</button></div></td>
+                        <td><div class="py-3 px-2"><button type="button" class="shadow-sm btn btn-danger btn-lg fw-bold border rounded-lg" data-bs-toggle="modal" data-bs-target="#verifModal">Done</button></div></td>
                     </tr>
                 @endforeach
             </tbody>
@@ -60,11 +60,38 @@
 </div>
 
 <script>
-    $(document).ready(function(){
-      $('#modalbutton').click(function(){
-        $('#verifModal').modal('show');
-      });
+    function sortTableByColumn(table, column, asc = true) {
+        const dirModifier = asc ? 1 : -1;
+        const tBody = table.tBodies[0];
+        const rows = Array.from(tBody.querySelectorAll("tr"));
+
+        const sortedRows = rows.sort((a, b) => {
+            const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+            const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+            return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+        });
+
+        while (tBody.firstChild) {
+            tBody.removeChild(tBody.firstChild);
+        }
+
+        tBody.append(...sortedRows);
+
+        table.querySelectorAll("th").forEach(th => th.classList.remove("th-asc", "th-desc"));
+        table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-asc", asc);
+        table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-desc", !asc);
+    }
+
+    document.querySelectorAll(".table-sortable th").forEach(headerCell => {
+        headerCell.addEventListener("click", () => {
+            const tableElement = headerCell.parentElement.parentElement.parentElement;
+            const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+            const currentIsAscending = headerCell.classList.contains("th-asc");
+            if(headerIndex != 3){
+                sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+            }
+        });
     });
-  </script>
+</script>
 
 @endsection
--- resources/views/penerimaanPengajuanKtm.blade.php
+++ resources/views/penerimaanPengajuanKtm.blade.php
@@ -6,7 +6,7 @@
     <h2 class="pt-5 fw-bold">Verifikasi Pengajuan KTM</h2>
     <!-- Table -->
     <div class="py-5">
-        <table class="table-list table-responsive table table-header text-center table-borderless hasil-table shadow">
+        <table id="dataTable" class="table-list table-sortable table-responsive table table-header text-center table-borderless hasil-table shadow">
             <thead class="h5 fw-bold align-middle m">
                 <tr>
                     <th class="w-25">Nama Mahasiswa</th>
@@ -32,5 +32,40 @@
         </div>
     </div>
 </div>
+
+<script>
+    function sortTableByColumn(table, column, asc = true) {
+        const dirModifier = asc ? 1 : -1;
+        const tBody = table.tBodies[0];
+        const rows = Array.from(tBody.querySelectorAll("tr"));
+
+        const sortedRows = rows.sort((a, b) => {
+            const aColText = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+            const bColText = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+            return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+        });
+
+        while (tBody.firstChild) {
+            tBody.removeChild(tBody.firstChild);
+        }
+
+        tBody.append(...sortedRows);
+
+        table.querySelectorAll("th").forEach(th => th.classList.remove("th-asc", "th-desc"));
+        table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-asc", asc);
+        table.querySelector(`th:nth-child(${column + 1})`).classList.toggle("th-desc", !asc);
+    }
+
+    document.querySelectorAll(".table-sortable th").forEach(headerCell => {
+        headerCell.addEventListener("click", () => {
+            const tableElement = headerCell.parentElement.parentElement.parentElement;
+            const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+            const currentIsAscending = headerCell.classList.contains("th-asc");
+            if(headerIndex != 3){
+                sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+            }
+        });
+    });
+</script>
 
 @endsection
