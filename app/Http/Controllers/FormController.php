b549a2f6e43c74301d89d008dfd85c0fed82683a
Alif T <alift929@gmail.com>
2024-01-03 01:08:48 +0700

    fix path

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Form;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class FormController extends Controller
 {
@@ -14,22 +15,26 @@ class FormController extends Controller
         $tipe = $request->input('tipe');
         
         $ksmfile = $request->file('ksm');
-        $path_ksm = $ksmfile->store('storage\app\public');
+        $path_ksm = 'public/file/ksm/' . pathinfo($ksmfile->getClientOriginalName(), PATHINFO_FILENAME) . '-' . auth()->user()->mahasiswa->nim . '.' . $ksmfile->extension();
+        Storage::put($path_ksm, file_get_contents($ksmfile));
         
         $path_surat_kehilangan = "";
         if ( $tipe ==  "Pengajuan Penggantian KTM"){
             $surat_kehilanganFile = $request->file('surat_kehilangan');
-            $path_surat_kehilangan = $surat_kehilanganFile->store('storage\app\public');
+            $path_surat_kehilangan = 'public/file/surat-kehilangan/' . pathinfo($surat_kehilanganFile->getClientOriginalName(), PATHINFO_FILENAME) . '-' . auth()->user()->mahasiswa->nim . '.' . $surat_kehilanganFile->extension();
+            Storage::put($path_surat_kehilangan, file_get_contents($surat_kehilanganFile));
         }
         
         $path_ktm = "";
         if ( $tipe !=  "Pengajuan Penggantian KTM"){
             $ktmFile = $request->file('ktm');
-            $path_ktm = $ktmFile->store('storage\app\public');
+            $path_ktm = 'public/file/ktm/' . pathinfo($ktmFile->getClientOriginalName(), PATHINFO_FILENAME) . '-' . auth()->user()->mahasiswa->nim . '.' . $ktmFile->extension();
+            Storage::put($path_ktm, file_get_contents($ktmFile));
         }
         
         $bukti_pembayaranFile = $request->file('bukti_pembayaran');
-        $path_bukti_pembayaran = $bukti_pembayaranFile->store('storage\app\public');
+        $path_bukti_pembayaran = 'public/file/bukti-pembayaran/' . pathinfo($bukti_pembayaranFile->getClientOriginalName(), PATHINFO_FILENAME) . '-' . auth()->user()->mahasiswa->nim . '.' . $bukti_pembayaranFile->extension();
+        Storage::put($path_bukti_pembayaran, file_get_contents($bukti_pembayaranFile));
         //
         $tipe = $request->input('tipe');
         $status = $request->input('status');
